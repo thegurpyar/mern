dc3129ab44523369d0a8f59cf0eeec83d32ea547
thegurpyar <gurpyar2828@gmail.com>
2025-03-20 13:57:14 +0530

    update

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,14 +1,15 @@
 import express from "express"
 import authMiddleware from "../middleware/auth.middleware.js";
-import { addPost,getAllPosts,editPost,deletePost } from "../controllers/post.controller.js";
+import { addPost,getAllPosts,editPost,deletePost, getPost } from "../controllers/post.controller.js";
 const router = express.Router();
 
 
 router.post("/", authMiddleware, addPost);
 
-// Get all posts
+
 router.get("/", getAllPosts);
 
+router.get("/:id", getPost);
 
 router.put("/:id", authMiddleware, editPost);
 
